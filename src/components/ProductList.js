6ede720aea0b2f7bd2911c9395e382800b06b711
Cameron Ashby <c00ashby@gmail.com>
2019-04-12 09:27:07 -0600

    finished tab filters

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 
-import MediaCard from './MediaCard';
 import SearchBar from './SearchBar'
 
 import '../CSS/productList.css';
@@ -18,26 +17,9 @@ class ProductList extends Component {
     };
 
     render() {
-        let items = this.props.apiData ? this.props.apiData : [];
-        let itemList = items.map((item, index) => {
-            return (
-                <div className='card'
-                     key={index}
-                >
-                    <MediaCard
-                        item={item}
-                        detail={true}
-                    />
-                </div>
-            )
-        });
         return (
             <div className='mainContainer'>
-                all or filter
-                <SearchBar stateTab={this.state.tab} changeTab={this.changeTab}/>
-                <div className='cardContainer'>
-                    {itemList}
-                </div>
+                <SearchBar itemArray={this.props.apiData} stateTab={this.state.tab} changeTab={this.changeTab}/>
             </div>
         );
     }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -6,6 +6,9 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 
+import * as _ from 'lodash';
+import MediaCard from "./MediaCard";
+
 function TabContainer(props) {
     return (
         <Typography component="div" style={{ padding: 8 * 3 }}>
@@ -23,12 +26,45 @@ const styles = theme => ({
         flexGrow: 1,
         backgroundColor: theme.palette.background.paper,
     },
+    tabContainer: {
+        display: 'flex',
+        justifyContent: 'center'
+    },
+    contentContainer: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'center'
+    }
 });
 
 class SimpleTabs extends React.Component {
     handleChange = (event, value) => {
         this.props.changeTab(value);
     };
+
+    mapFilterArray(array) {
+        return array.map((item, index) => {
+            return (
+                <div className='card'
+                     key={index}
+                >
+                    <MediaCard
+                        item={item}
+                        detail={true}
+                    />
+                </div>
+            )
+        })
+    }
+
+    sortChange(choice) {
+        if(choice === 'all') {
+            return this.props.itemArray;
+        }
+        else {
+            return _.filter(this.props.itemArray, {'category': choice})
+        }
+    }
 
     render() {
         const { classes } = this.props;
@@ -37,7 +73,8 @@ class SimpleTabs extends React.Component {
         return (
             <div className={classes.root}>
                 <AppBar position="static">
-                    <Tabs value={value.value} onChange={this.handleChange}>
+                    <Tabs className={classes.tabContainer} value={value.value} onChange={this.handleChange}>
+                        <Tab label="All" />
                         <Tab label="Phones" />
                         <Tab label="TVs" />
                         <Tab label="Appliances" />
@@ -46,12 +83,48 @@ class SimpleTabs extends React.Component {
                         <Tab label="Action-Cameras" />
                     </Tabs>
                 </AppBar>
-                {value.value === 0 && <TabContainer>Phones</TabContainer>}
-                {value.value === 1 && <TabContainer>TVs</TabContainer>}
-                {value.value === 2 && <TabContainer>Appliances</TabContainer>}
-                {value.value === 3 && <TabContainer>Refrigerators</TabContainer>}
-                {value.value === 4 && <TabContainer>Watches</TabContainer>}
-                {value.value === 5 && <TabContainer>Action-Cameras</TabContainer>}
+                {value.value === 0 &&
+                <TabContainer>
+                    <div className={classes.contentContainer}>
+                        {this.mapFilterArray(this.sortChange('all'))}
+                    </div>
+                </TabContainer>}
+                {value.value === 1 &&
+                <TabContainer>
+                    <div className={classes.contentContainer}>
+                        {this.mapFilterArray(this.sortChange('phone'))}
+                    </div>
+                </TabContainer>}
+                {value.value === 2 &&
+                <TabContainer>
+                    <div className={classes.contentContainer}>
+                        {this.mapFilterArray(this.sortChange('tv'))}
+                    </div>
+                </TabContainer>}
+                {value.value === 3 &&
+                <TabContainer>
+                    <div className={classes.contentContainer}>
+                        {this.mapFilterArray(this.sortChange('small-appliance'))}
+                    </div>
+                </TabContainer>}
+                {value.value === 4 &&
+                <TabContainer>
+                    <div className={classes.contentContainer}>
+                        {this.mapFilterArray(this.sortChange('refrigerator'))}
+                    </div>
+                </TabContainer>}
+                {value.value === 5 &&
+                <TabContainer>
+                    <div className={classes.contentContainer}>
+                        {this.mapFilterArray(this.sortChange('watch'))}
+                    </div>
+                </TabContainer>}
+                {value.value === 6 &&
+                <TabContainer>
+                    <div className={classes.contentContainer}>
+                        {this.mapFilterArray(this.sortChange('action-camera'))}
+                    </div>
+                </TabContainer>}
             </div>
         );
     }
